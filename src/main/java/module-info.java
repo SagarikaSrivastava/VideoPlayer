2a8405d1533767057ab3dd078278338846fcf891
Sagarika Srivastava <sgrksrivastava@gmail.com>
2024-11-16 00:39:52 +0530

    Media Player added. Controls yet to be added.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.videoplayer {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
 
     requires org.kordamp.bootstrapfx.core;
 
--- src/main/java/org/example/videoplayer/HelloApplication.java
+++ src/main/java/org/example/videoplayer/HelloApplication.java
@@ -2,19 +2,41 @@ package org.example.videoplayer;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 
 public class HelloApplication extends Application {
+
+    private final File videoFile = new File("C:\\Users\\Sagarika\\Videos\\sample1.mp4");
+
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Video Player");
+        Group root = new Group();
+        Scene scene = new Scene(root, 540, 210);
         stage.setScene(scene);
+        stage.sizeToScene();
         stage.show();
+
+        Media media = new Media(videoFile.toURI().toString());
+        MediaPlayer player = new MediaPlayer(media);
+        player.setAutoPlay(true);
+        MediaView mediaView = new MediaView(player);
+        ((Group) scene.getRoot()).getChildren().add(mediaView);
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        stage.setTitle("Hello!");
+//        stage.setScene(scene);
+//        stage.show();
     }
 
     public static void main(String[] args) {
